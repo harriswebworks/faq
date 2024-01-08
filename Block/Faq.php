9bd4e3bfada616ec2cba18c4990dc33cec46ac4e
ferdous2256 <mdferdousofficial@gmail.com>
2024-01-08 19:15:06 +0600

    front end

---
+++ Block/Faq.php
@@ -0,0 +1,32 @@
+<?php
+namespace Harriswebworks\FAQ\Block;
+
+use Harriswebworks\FAQ\Model\ResourceModel\FAQ\Collection as FaqItemCollection;
+use Harriswebworks\FAQ\Model\ResourceModel\Category\Collection as FaqCategoryCollection;
+
+class Faq extends \Magento\Framework\View\Element\Template
+{
+    protected $faqItemCollection;
+    protected $faqCategoryCollection;
+
+    public function __construct(
+        \Magento\Framework\View\Element\Template\Context $context,
+        FaqItemCollection $faqItemCollection,
+        FaqCategoryCollection $faqCategoryCollection,
+        array $data = []
+    ) {
+        parent::__construct($context, $data);
+        $this->faqItemCollection = $faqItemCollection;
+        $this->faqCategoryCollection = $faqCategoryCollection;
+    }
+
+    public function getFaqItems()
+    {
+        return $this->faqItemCollection->getItems();
+    }
+
+    public function getFaqCategories()
+    {
+        return $this->faqCategoryCollection->getItems();
+    }
+}
