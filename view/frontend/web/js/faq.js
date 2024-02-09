fd492fed73206f5c541f36e43e288e87589bb513
alhussain50 <alhussain@iut-dhaka.edu>
2024-02-09 15:57:22 +0600

    custom external js file added

---
+++ view/frontend/web/js/faq.js
@@ -0,0 +1,35 @@
+define(['jquery'], function ($) {
+    'use strict';
+    // Hide all answers initially
+    $('.hww-faq-answer').hide();
+
+    // Show the selected category section on category link click
+    $('.category-link').on('click', function (event) {
+        event.preventDefault();
+
+        $(".faq-item").show();
+        $(".faq-item").removeClass("active")
+        $(".hww-faq-category-name").show();
+        $('.hww-faq-answer').hide();
+
+        var categoryId = $(this).data('category-id');
+
+        if (categoryId === 'show-all') {
+            // Show all category sections
+            $('.category-section').show();
+        } else {
+            // Hide all other category sections
+            $('.category-section').hide();
+
+            // Show the selected category section
+            $('#category-' + categoryId).show();
+        }
+    });
+
+    // Toggle answer visibility on question click
+    $(".faq-item").on("click", function () {
+        $(this).find(".hww-faq-answer").slideToggle(500);
+        $(this).toggleClass("active");
+
+    });
+});
