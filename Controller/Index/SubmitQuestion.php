62ca6d62a61fa87e3eb955c85267ea3736a5cffd
alhussain50 <alhussain@iut-dhaka.edu>
2024-03-01 18:05:04 +0600

    route error fixing

--- Controller/Index/SubmitQuestion.php
+++ Controller/Index/SubmitQuestion.php
@@ -6,23 +6,32 @@ namespace Harriswebworks\FAQ\Controller\Index;
 use Magento\Framework\App\Action\Action;
 use Magento\Framework\App\Action\Context;
 use Harriswebworks\FAQ\Model\FaqFactory;
+use Harriswebworks\FAQ\Helper\Data as CustomRouteHelper;
+
 
 class SubmitQuestion extends Action
 {
     protected $faqItemFactory;
 
+    /**
+     * @var CustomRouteHelper
+     */
+    protected $helper;
+
     public function __construct(
         Context $context,
+        CustomRouteHelper $helper,
         FaqFactory $faqItemFactory
     ) {
         parent::__construct($context);
         $this->faqItemFactory = $faqItemFactory;
+        $this->helper = $helper;
     }
 
     public function execute()
     {
         $postData = $this->getRequest()->getPostValue();
-        
+
         if ($postData) {
             $categoryId = $postData['category'];
 
@@ -33,14 +42,16 @@ class SubmitQuestion extends Action
                 'answer' => '', // You can set an empty answer or handle it as needed
                 'category_id' => $categoryId,
             ]);
-            
+
             // Save the FAQ item
             $faqItem->save();
 
             $this->messageManager->addSuccessMessage(__('Your question has been submitted successfully.'));
         }
 
+        $route = $this->helper->getModuleRoute();
+
         // Redirect back to the FAQ page
-        $this->_redirect('faq/');
+        $this->_redirect($route . '/');
     }
 }
